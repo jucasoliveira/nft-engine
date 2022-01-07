d59f7d2515892faec15ede14b528d93b817e323d
jucasoliveira <jucas.oliveira@gmail.com>
2022-01-07 10:22:11 +0000

    just to

--- project/pages/about.tsx
+++ project/pages/about.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 const AboutPage = () => (
   <Layout title="About | Next.js + TypeScript Example">
     <h1>About</h1>
-    <p>This is the about page</p>
+    <p>This is the about page 2</p>
     <p>
       <Link href="/">
         <a>Go home</a>
